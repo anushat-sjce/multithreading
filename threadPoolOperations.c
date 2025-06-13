a0c3c1a9e1f282036b202ee7387bf9493e9518af
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-13 10:17:41 +0530

    Create threadPoolOperations.c

---
+++ threadPoolOperations.c
@@ -0,0 +1,10 @@
+#Thread pool is a design technique to create set of threads to be ready state when a task is submitted.
+#this helps in reuse of threads to execute tasks concurrently.
+#instead of creating threads for every tasks, thread pool creates and maintains a collection of pre initialised threads.
+#any thread is picked from the pool and task is initialised.
+
+# Thread pool - Collection of pre initialised threads that are waiting for tasks to be assigned.
+# Worker threads
+# Task queue
+# Task execution
+# Task submission
